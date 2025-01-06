4084d4805159e6bc6220dc9cec8915fe0e4ae15c
Barbara Geller <barbara@copperspice.com>
2025-01-06 08:36:10 -0800

    Update copyright

--- cmake/modules/Load_Catch2.cmake
+++ cmake/modules/Load_Catch2.cmake
@@ -1,12 +1,12 @@
 # ***********************************************************************
 #
-# Copyright (c) 2021-2024 Barbara Geller
-# Copyright (c) 2021-2024 Ansel Sermersheim
+# Copyright (c) 2021-2025 Barbara Geller
+# Copyright (c) 2021-2025 Ansel Sermersheim
 #
 # This file is part of CsCrypto.
 #
-# CsCrypto is free software, released under the BSD 2-Clause license.
-# For license details refer to LICENSE provided with this project.
+# CsCrypto is free software which is released under the BSD 2-Clause license.
+# For license details refer to the LICENSE provided with this project.
 #
 # CsCrypto is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
